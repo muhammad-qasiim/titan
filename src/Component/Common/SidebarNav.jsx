73531535f0f008836c633f882e3c164ee0c616ce
qasim16 <qasim@sociabletech.com.au>
2022-06-21 12:58:49 +0500

    navbar UI

--- src/Component/Common/SidebarNav.jsx
+++ src/Component/Common/SidebarNav.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import Sidebar from './Sidebar';
+import { CgProfile } from "react-icons/cg";
 import { Link } from 'react-router-dom';
 import { AiFillFacebook, AiFillTwitterSquare,AiFillInstagram  } from "react-icons/ai";
 import { BsDiscord, BsSignal  } from "react-icons/bs";
 
 const SidebarNav = ({ toggleSidebarNav, setToggleSidebarNav }) => {
     const [toggleSidebar, setToggleSidebar] = useState(false);
-    const Navigation = [{ placeholder: 'Events', path: '/events' }, { placeholder: 'Profile', path: '/profile' },]
+    const Navigation = [{ placeholder: 'Home', path: '/home' }, { placeholder: 'Events', path: '/events' }, { placeholder: 'Airdrop Challenge', path: '/challenge' }, { placeholder: 'Profile', path: '/profile' },]
 
     return (
         <>
@@ -24,6 +25,16 @@ const SidebarNav = ({ toggleSidebarNav, setToggleSidebarNav }) => {
                                 </li>
                             ))
                         }
+                    </ul>
+
+                    <ul>
+                    <Link to="/signin">
+                                <li onClick={() => setToggleSidebarNav(false)}>
+                                    <span className='relative flex justify-center items-center text-3xl border-b-2 border-transparent cursor-pointer transition-all text-gray-500 hover:text-gray-800'>
+                                        <CgProfile />
+                                    </span>
+                                </li>
+                            </Link>
                     </ul>
 
                     <section className='gGngoX'>
