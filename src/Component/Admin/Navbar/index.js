79f06f30a8f9c0a579ef50b17ff0d421cd40fbc4
mac <mac@macs-MacBook-Air.local>
2022-06-23 14:55:42 +0500

    change merge

--- src/Component/Admin/Navbar/index.js
+++ src/Component/Admin/Navbar/index.js
@@ -9,9 +9,10 @@ const Navbar = () => {
                         <Link to="/admin/dashboard">
                             <span className="logo text-xl text-red-500 font-semibold whitespace-nowrap flex items-center gap-2">
                                 <img className="w-36" src="/assets/image/beglobal.svg" alt="" />
-                                Titan</span>
+                                Admin Titan
+                            </span>
                         </Link>
-                        <button className="bg-red-500 text-white px-32 py-8 rounded-5 transition-all hover:bg-red-600 relative top-0 hover:top-px" >Connect</button>
+                        {/* <button className="bg-red-500 text-white px-32 py-8 rounded-5 transition-all hover:bg-red-600 relative top-0 hover:top-px" >Connect</button> */}
                 </section>
             </nav>
         </>
--- src/pages/Admin/CreateMission/index.js
+++ src/pages/Admin/CreateMission/index.js
@@ -2,19 +2,67 @@ import { BsFileImage, BsAsterisk } from "react-icons/bs";
 import Input from '../../../Component/Common/Input';
 import Navbar from '../../../Component/Admin/Navbar';
 import Sidebar from '../../../Component/Admin/Sidebar';
+import { useState } from "react";
+import isEmpty from "../../../utils/isEmpty";
 
 const CreateMission = () => {
+    const [userData, setUserData] = useState({ name: '', description: '', imageUrl: '', startDate: '', endDate: '', startTime: '', endTime: '' });
+    const { name, description, imageUrl, startDate, endDate, startTime, endTime } = userData;
+    const [errors, setErrors] = useState({});
+    const handleChange = event => {
+        const { name, value } = event.target;
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0];
+            setUserData({
+                ...userData,
+                [name]: img,
+            });
+        } else {
+            setUserData({ ...userData, [name]: value });
+        }
+        setErrors({});
+    }
 
+    const validate = () => {
+        const _errors = {};
+        if (isEmpty(name)) {
+            _errors.name = 'Please enter name.';
+        }
+        if (isEmpty(startDate)) {
+            _errors.startDate = 'Please enter start date.';
+        }
+        if (isEmpty(startTime)) {
+            _errors.startTime = 'Please enter start time.';
+        }
+        if (isEmpty(endTime)) {
+            _errors.endTime = 'Please enter end time.';
+        }
+        if (isEmpty(endDate)) {
+            _errors.endDate = 'Please enter end date.';
+        }
+        if (isEmpty(imageUrl)) {
+            _errors.imageUrl = 'Please upload image.';
+        }
+        if (isEmpty(description)) {
+            _errors.description = 'Please enter description.';
+        }
+        return _errors;
+    }
+
+    const _createMission = () => {
+        const errors = validate();
+        if (isEmpty(errors)) {
+            // login(userData);
+        }
+        setErrors(errors || {});
+    }
     return (
         <>
             <Navbar />
-
             <main className="flex gap-3">
-
                 <section className="w-420">
                     <Sidebar />
                 </section>
-
                 <section className="flex flex-col flex-1 items-center justify-center">
                     <section className="createItemContainer container mx-auto px-24 lg:px-99 mt-28 mb-100  w-full">
                         <h3 className="text-40 font-semibold text-left my-42">Add New Mission</h3>
@@ -24,16 +72,23 @@ const CreateMission = () => {
                             label="Mission name"
                             type='text'
                             name="name"
+                            value={name}
+                            handleChange={handleChange}
+                            errorMessage={errors?.name}
                         />
                         <div className='mb-22'>
                             <label className="text-gray-800 font-medium" htmlFor="#">Mission description</label> <br />
                             <p className='caption-text mt-6 mb-10'>The description will be included on the item's detail page underneath its image. Markdown syntax is supported.</p>
                             <textarea
-                                className={`w-full py-18 text-14 px-16 h-176`}
+                                className={`w-full py-18 text-14 px-16 h-176 ${errors?.description && 'input-error'}`}
                                 placeholder='Provide a detailed description of your item.'
                                 name="describtion"
+                                value={description}
+                                onChange={handleChange}
+                                errorMessage={errors?.description}
                             >
                             </textarea>
+                            {errors?.description && <p className="text-red-700 text-10 mt-4 ml-2"> {errors?.description} </p>}
                         </div>
                         <div className='mb-32'>
                             <label className="text-gray-800 font-medium mb-6 flex items-start gap-1" htmlFor="#">Mission Cover Image <BsAsterisk className="text-8 text-red-600 relative top-1" /></label>
@@ -43,42 +98,65 @@ const CreateMission = () => {
                                     id="file-input"
                                     className={`absolute inset-0 opacity-0 cursor-pointer`}
                                     type="file"
+                                    name='imageUrl'
                                     accept=" .jpg , .jpeg , .png , .gif, .mp4, .mp3, .mpeg, .mov, video/quicktime"
                                 />
-
-                                <BsFileImage className="text-56" style={{ color: 'rgb(179, 179, 179)' }} />
+                                {imageUrl ?
+                                    <>
+                                        {/* {image?.type?.split('/')?.includes('video') ?
+                                            <video width="320" height="240" controls>
+                                                <source src={imageUrl} type="video/mp4" />
+                                            </video> : */}
+                                        <img src={URL.createObjectURL(imageUrl && imageUrl)} alt="" />
+                                        {/* } */}
+                                    </>
+                                    : <BsFileImage className="text-56" style={{ color: 'rgb(179, 179, 179)' }} />
+                                }
                             </section>
+                            {errors?.imageUrl && <p className="text-red-700 text-10 mt-4 ml-2"> {errors?.imageUrl} </p>}
                         </div>
                         <div className='flex items-center gap-3 mb-18'>
                             <Input
                                 className="mb-22"
                                 label="Mission start date"
                                 type='date'
-                                name="name"
+                                name="startDate"
+                                value={startDate}
+                                handleChange={handleChange}
+                                errorMessage={errors?.startDate}
                             />
                             <Input
                                 className="mb-22"
                                 type='time'
                                 label='Mission start time'
-                                name="name"
+                                name="endDate"
+                                value={endDate}
+                                handleChange={handleChange}
+                                errorMessage={errors?.endDate}
                             />
                         </div>
 
                         <div className='flex items-center gap-3 mb-18'>
                             <Input
                                 className="mb-22"
-                                label="Mission end date"
+                                label="Event end date"
                                 type='date'
-                                name="name"
+                                name="startTime"
+                                value={startTime}
+                                handleChange={handleChange}
+                                errorMessage={errors?.startTime}
                             />
                             <Input
                                 className="mb-22"
                                 type='time'
-                                label='Mission end time'
-                                name="name"
+                                label='Event end time'
+                                name="endTime"
+                                value={endTime}
+                                handleChange={handleChange}
+                                errorMessage={errors?.endTime}
                             />
                         </div>
-                        <div className='mb-18 w-2/4'>
+                        <div className='mb-18 w-4/4'>
                             <label className="text-gray-800 font-medium" htmlFor="#">Select Event</label> <br />
                             <select name="category" className='w-full py-18 pl-18 text-14 pr-16 mt-8'>
                                 <option value="Collectibles">Collectibles</option>
@@ -92,12 +170,10 @@ const CreateMission = () => {
                             </select>
                         </div>
                         <hr />
-                        <button className="bg-red-500 text-white px-32 py-10 mt-52 rounded-5 transition-all hover:bg-red-600 relative top-0 hover:top-px"> Add </button>
+                        <button onClick={() => _createMission()} className="bg-red-500 text-white px-32 py-10 mt-52 rounded-5 transition-all hover:bg-red-600 relative top-0 hover:top-px"> Create Mission </button>
                     </section>
                 </section>
-
             </main>
-
         </>
     )
 }
